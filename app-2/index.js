bc1cd8bd7892221b39b51f042119f865c2eb3659
Max Tulian <maxtulian93@gmail.com>
2021-02-12 23:50:07 -0300

    Add update

--- app-2/index.js
+++ app-2/index.js
@@ -2,9 +2,11 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const { v4: uuid } = require('uuid');
+const methodOverride = require('method-override');
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
+app.use(methodOverride('_method'))
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
@@ -34,6 +36,12 @@ app.get('/comments/:id', (req, res) => {
   const { id } = req.params;
   const comment = comments.find(c => c.id === id);
   res.render('comments/show', { comment });
+});
+
+app.get('/comments/:id/edit', (req, res) => {
+  const { id } = req.params;
+  const comment = comments.find(c => c.id === id);
+  res.render('comments/edit', { comment });
 });
 
 app.patch('/comments/:id', (req, res) => {
